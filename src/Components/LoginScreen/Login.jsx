b71a1a892ce1bc663dbc04232b0177249b712a0e
yamil351 <leokasper@live.com.ar>
2023-02-24 14:25:26 -0300

    navbar-desing

--- src/Components/LoginScreen/Login.jsx
+++ src/Components/LoginScreen/Login.jsx
@@ -1,27 +1,24 @@
-import React from "react";
-import { useState } from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
 const Section = styled.section`
   width: 100%;
   justify-content: center;
   align-items: center;
-`;
-const Container = styled.div`
-
-`;
+`
+const Container = styled.div``
 
 function Login() {
-  const [Log, setLog] = useState(false);
+ 
 
-  return (
-    <Section>
-      <Container>
-        <form></form>
-        <button></button>
-      </Container>
-    </Section>
-  );
+    return (
+        <Section id='SignIn'>
+            <Container>
+                <form></form>
+                <button></button>
+            </Container>
+        </Section>
+    )
 }
 
-export default Login;
+export default Login
--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -3,47 +3,71 @@ import { data } from './data'
 import { loged } from './data'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import {MdMenu} from 'react-icons/md'
+import './navbar.css'
 
 const NavbarContainer = styled.nav`
-  width: 10rem;
+  width: 14rem;
   height: 100vh;
   position: fixed;
   z-index: 100;
-  background-color: #222;
+  background-color: var(--clr-primary-1);
+  alig-items: center;
+  justify-content: center;
 `
-const Loged = styled.div``
+const Loged = styled.div`
+  line-height: 3.4rem;
+`
+
 const List = styled.ul`
   list-style: none;
 `
-const LinkContainer = styled.li`
+const LinkContainer = styled.li``
 
+const UnLoged = styled.div`
+  line-height: 3.4rem;
 `
-
-const UnLoged = styled.div``
+const UserPhoto = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background-color: var(--clr-grey-1);
+  justify-content: center;
+  alig-items: center;
+  margin: 2rem auto;
+  border-radius: 20px;
+`
+const UserProfile = styled.div``
+const Username = styled.h1`
+  color: var(--clr-primary-3);
+  text-align: center;
+  justify-content: center;
+  margin: auto;
+  font-seize: 2rem;
+`
+const Menu= styled.button`
+color: var(--clr-primary-3);
+background-color:var(--clr-primary-1);
+font-size:2rem;
+border:none;
+`
 function Navbar() {
     return (
         <NavbarContainer>
-            <Loged>
-                <List>
-                    {loged.map((log) => {
-                        const { id, name, icon, url } = log
-                        return (
-                            <LinkContainer key={id}>
-                                <Link to={url}>
-                                    {icon} {name}
-                                </Link>
-                            </LinkContainer>
-                        )
-                    })}
-                </List>
-            </Loged>
+            <Menu>
+                <MdMenu/>
+            </Menu>
+            <UserProfile>
+                <UserPhoto></UserPhoto>
+                <Username>Username</Username>
+            </UserProfile>
+
             <UnLoged>
                 <List>
                     {data.map((links) => {
                         const { id, name, icon, url } = links
                         return (
                             <LinkContainer key={id}>
-                                <Link to={url}>
+                                <Link to={url} className="link-list">
                                     {icon} {name}
                                 </Link>
                             </LinkContainer>
@@ -51,6 +75,20 @@ function Navbar() {
                     })}
                 </List>
             </UnLoged>
+            <Loged>
+                <List>
+                    {loged.map((log) => {
+                        const { id, name, icon, url } = log
+                        return (
+                            <LinkContainer key={id}>
+                                <Link to={url} className="link-list">
+                                    {icon} {name}
+                                </Link>
+                            </LinkContainer>
+                        )
+                    })}
+                </List>
+            </Loged>
         </NavbarContainer>
     )
 }
