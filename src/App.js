ffae263c5a5e937a8b391b7fb8c994b76fcbd411
yamil351 <leokasper@live.com.ar>
2023-02-24 13:25:25 -0300

    linter-added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
-import Navbar from "./Components/navbar/navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Components/LoginScreen/Login.jsx";
+import React, { Component } from 'react'
+import './App.css'
+import Navbar from './Components/navbar/navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './Components/LoginScreen/Login.jsx'
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/SignIn" element={<Login/>}/>
-        </Routes>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/SignIn" element={<Login />} />
+                </Routes>
+            </Router>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { data } from "./data";
-import { loged } from "./data";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { data } from './data'
+import { loged } from './data'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const NavbarContainer = styled.nav`
   width: 10rem;
@@ -10,48 +10,48 @@ const NavbarContainer = styled.nav`
   position: fixed;
   z-index: 100;
   background-color: #222;
-`;
-const Loged = styled.div``;
+`
+const Loged = styled.div``
 const List = styled.ul`
   list-style: none;
-`;
+`
 const LinkContainer = styled.li`
 
-`;
+`
 
-const UnLoged = styled.div``;
+const UnLoged = styled.div``
 function Navbar() {
-  return (
-    <NavbarContainer>
-      <Loged>
-        <List>
-          {loged.map((log) => {
-            const { id, name, icon, url } = log;
-            return (
-              <LinkContainer key={id}>
-                <Link to={url}>
-                  {icon} {name}
-                </Link>
-              </LinkContainer>
-            );
-          })}
-        </List>
-      </Loged>
-      <UnLoged>
-        <List>
-          {data.map((links) => {
-            const { id, name, icon, url } = links;
-            return (
-              <LinkContainer key={id}>
-                <Link to={url}>
-                  {icon} {name}
-                </Link>
-              </LinkContainer>
-            );
-          })}
-        </List>
-      </UnLoged>
-    </NavbarContainer>
-  );
+    return (
+        <NavbarContainer>
+            <Loged>
+                <List>
+                    {loged.map((log) => {
+                        const { id, name, icon, url } = log
+                        return (
+                            <LinkContainer key={id}>
+                                <Link to={url}>
+                                    {icon} {name}
+                                </Link>
+                            </LinkContainer>
+                        )
+                    })}
+                </List>
+            </Loged>
+            <UnLoged>
+                <List>
+                    {data.map((links) => {
+                        const { id, name, icon, url } = links
+                        return (
+                            <LinkContainer key={id}>
+                                <Link to={url}>
+                                    {icon} {name}
+                                </Link>
+                            </LinkContainer>
+                        )
+                    })}
+                </List>
+            </UnLoged>
+        </NavbarContainer>
+    )
 }
-export default Navbar;
+export default Navbar
