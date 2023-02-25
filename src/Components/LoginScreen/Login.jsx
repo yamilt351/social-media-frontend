90b889ffd8cd468afe713a2f54da1029a8701bd3
yamil351 <leokasper@live.com.ar>
2023-02-24 21:19:00 -0300

    login-signupscreen-done

--- src/Components/LoginScreen/Login.jsx
+++ src/Components/LoginScreen/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './login.css'
 
 function Login() {
-    const[register, setRegister] = useState(true)
+    const [register, setRegister] = useState(true)
 
     function swicht() {
         if (register) {
@@ -16,26 +16,39 @@ function Login() {
     return (
         <section id="SignIn" className="SignIn">
             <h1>Welcome!</h1>
-            {register? 
-                <div className='form-container signUp'>
-                    <form className='form'>
-                        <input placeholder='email' type="email"></input>
-                        <input placeholder='username' type="text"></input>
-                        <input type="password" placeholder='password'/>
-                    </form>
-                    <button type='submit' className='action-button'> Create User</button>
-                </div>: <div className='form-container signIn'>
-                    <form className='form'>
-                        <input placeholder='username' type="text"></input>
-                        <input type="password" placeholder='password'/>
-                    </form>
-                    <button type='submit' className='action-button'> Login </button>
-                </div>
-            }
-            <button className='swichter-button' onClick={()=>swicht()}>
-                {register? 'Do you have an account? Sign In' : 'Dont have an account? Sign Up!'}
 
-            </button>
+            <div className="SignIn-Container">
+                {register ? (
+                    <div className="form-container signUp">
+                        <form className="form">
+                            <input placeholder="email" type="email"></input>
+                            <input placeholder="username" type="text"></input>
+                            <input type="password" placeholder="password" />
+                        </form>
+                        <button type="submit" className="action-button">
+                            {' '}
+              Create User
+                        </button>
+                        <button className="swichter-button" onClick={() => swicht()}>
+                            {register ? 'Sign In' : 'Sign Up!'}
+                        </button>
+                    </div>
+                ) : (
+                    <div className="form-container signIn">
+                        <form className="form">
+                            <input placeholder="username" type="text"></input>
+                            <input type="password" placeholder="password" />
+                        </form>
+                        <button type="submit" className="action-button">
+                            {' '}
+              Login{' '}
+                        </button>
+                        <button className="swichter-button" onClick={() => swicht()}>
+                            {register ? 'Sign In' : 'Sign Up!'}
+                        </button>
+                    </div>
+                )}
+            </div>
         </section>
     )
 }
