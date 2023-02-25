271dd7401d0e1c351f8a214e14b1eb9b3849ea5c
yamil351 <leokasper@live.com.ar>
2023-02-25 15:40:24 -0300

    createPost-mediaqueries-fixed

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -11,16 +11,16 @@ function CreatePost() {
                     <form action="" className="form-post">
                         <label htmlFor=""><MdTipsAndUpdates/> Title</label>
                         <input type="text" placeholder="Title" />
-                        <label htmlFor=""><MdEditNote/> Describe in a few words your story</label>
-                        <input type="text" placeholder="Description" />
+                        <label htmlFor=""><MdEditNote/> Sinopsis</label>
+                        <input type="text" placeholder="Sinopsis" />
                         <label><MdChatBubble/> Write your Post here!</label>
-                        <input type="textbox" placeholder="Post" rows="30"/>
+                        <textarea type="textarea" placeholder="Post" rows="10"/>
                         <label htmlFor=""><MdInsertPhoto/> Thumbnail</label>
                         <input type="file" placeholder="Thumbnail" />
                         <label htmlFor=""><MdSearch/> Make easy to find your post using tags!</label>
                         <input type="text" placeholder=" separate tags by comma" />
                     </form>
-                    <button>Ready!</button>
+                    <button className='button-post'>Ready!</button>
                 </div>
             </div>
         </section>
