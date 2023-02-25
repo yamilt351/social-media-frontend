58aecb1c3b81da059649d438ceac41ef8a1ebc6e
yamil351 <leokasper@live.com.ar>
2023-02-25 02:18:56 -0300

    card-added

---
+++ src/Components/CardContainer/CardContainer.jsx
@@ -0,0 +1,37 @@
+import React from 'react' 
+import './CardContainer.css'
+import Card from '../Card/card'
+
+
+function CardContainer() {
+    return (
+        <section className='section-card-container'>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+        </section>
+
+    )
+}
+export default CardContainer
---
+++ src/Components/CreatePost/data.js
@@ -0,0 +1,9 @@
+const data = [
+    {
+        id: 1,
+        description: 'Describe shortly your story',
+        title: 'Create Story',
+        img: 'Thumdnail',
+        tags: 'use tags to make easier find your story, separete them with commas.',
+    },
+]
