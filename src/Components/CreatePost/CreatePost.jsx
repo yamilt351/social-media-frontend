c6431c57e73f8269003b383e3a2c4310a58ed20a
yamil351 <leokasper@live.com.ar>
2023-02-25 02:44:36 -0300

    form-builded

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -1,4 +1,31 @@
 import React from 'react'
-
-function CreatePost() {}
+import data from './data'
+function CreatePost() {
+    return (
+        <section className="create-Post">
+            <div>
+                {data.map((value) => {
+                    const { id, description, title, post, img, tags, button } = value
+                    return (
+                        <div className="create-post" key={id}>
+                            <form action="" className="form-post">
+                                <label htmlFor="">{title}</label>
+                                <input type="text" placeholder={title} />
+                                <label htmlFor="">{description}</label>
+                                <input type="text" placeholder={description} />
+                                <label>{post}</label>
+                                <input type="text" placeholder={post} />
+                                <label htmlFor="">{img}</label>
+                                <input type="text" placeholder={img} />
+                                <label htmlFor="">{tags}</label>
+                                <input type="text" placeholder={tags} />
+                            </form>
+                            <button>{button}</button>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
+    )
+}
 export default CreatePost
--- src/Components/CreatePost/data.js
+++ src/Components/CreatePost/data.js
@@ -1,9 +1,24 @@
+import React from 'react'
+import {
+    MdAddPhotoAlternate,
+    MdCheckCircleOutline,
+    MdSpeakerNotes,
+    MdTextFields,
+    MdTitle,
+} from 'react-icons/md'
+
+MdCheckCircleOutline
 const data = [
     {
         id: 1,
-        description: 'Describe shortly your story',
-        title: 'Create Story',
-        img: 'Thumdnail',
+        description: <MdTextFields /> + 'Describe shortly your story',
+        title: <MdTitle /> + 'Tittle',
+        post: <MdSpeakerNotes /> + 'Share your story here',
+        img: <MdAddPhotoAlternate /> + 'Thumdnail',
         tags: 'use tags to make easier find your story, separete them with commas.',
+
+        button: 'Ready!' + <MdCheckCircleOutline />,
     },
 ]
+
+export default data
