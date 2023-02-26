0f45fe4c29e41c1ca3aea6e67117062ff2c239bd
Luis Franco <lafrancol@unitecnica.net>
2023-02-25 19:49:57 -0500

    add comments and content-comments

---
+++ src/Components/contentComments/ContentComments.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Comment from './coments/Comment'
+import './index.css'
+
+
+const ContentComments = () => {
+  return (
+    <div className='content'>
+        <Comment/>
+    </div>
+  )
+}
+
+export default ContentComments
---
+++ src/Components/contentComments/coments/Comment.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+
+const Comment = () => {
+  return (
+    <aside className='comment'>
+    <head className='comment-head'>
+        <img src="" alt="" />
+        <span className='username'>username</span>
+    </head>
+    <form action="" className='comment-form'>
+        <input type="text" />
+        <button className='comment-form-btn'>Created at</button>
+    </form>
+    </aside>
+  )
+}
+
+export default Comment
