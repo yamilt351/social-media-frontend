8586946e0bba03129d5f911ec5f662ab3df1cd7d
yamil351 <leokasper@live.com.ar>
2023-02-27 15:40:13 -0300

    profile-view-created

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
 import './Profile.css'
-
-
-
+import Card from '../Card/card.jsx'
 function Profile() {
     return (
         <section className="profile-container-main" id="profile">
-            <h1>Profile</h1>
+            <section className="user-profile-window-container">
+                <div className="user-information-container">
+                    <div className="user-image-container">
+                        <img
+                            src="adasd"
+                            alt="user-image-profile"
+                            className="user-image-profile"
+                        />
+                    </div>
+                    <div className="user-data-container">
+                        <div className="user contact-information-container">
+                            <div className="username-container-profile">
+                                <h1 className="username-profile">username</h1>
+                            </div>
+                            <div className="email-container-profile">
+                                <h2 type="text" className="email-profile">
+                  userEmail@hotmail.com
+                                </h2>
+                            </div>
+                            <div className="createdat-container-profile">
+                                <h2 type="text" className="created-profile">
+                  created : 25 days ago
+                                </h2>
+                            </div>
+                            <div className="followers-container">
+                                <h3 className="followers-user">followed by 23000000 readers</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="following-users">
+                <h1 className="following-users-title">following:</h1>
+                <div className="following-users-container">
+                    <div className="user-followed-container">
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                    </div>
+                </div>
+            </section>
         </section>
     )
 }
