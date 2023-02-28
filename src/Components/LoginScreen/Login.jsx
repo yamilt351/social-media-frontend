a02fb0c64a939b09ff73cffb5dbccb78ab861f73
yamil351 <leokasper@live.com.ar>
2023-02-28 12:31:53 -0300

    axios-installed

--- src/Components/LoginScreen/Login.jsx
+++ src/Components/LoginScreen/Login.jsx
@@ -14,14 +14,10 @@ function Login() {
     }
 
     return (
-<<<<<<< HEAD
         <section id="SignIn" className="SignIn">
             <div className="SignIn-Container">
                 <h1>Welcome!</h1>
-=======
-        <section id="SignIn" className="login-screen">
-            <div className="form-action-container">
->>>>>>> 4ad3eda (axios-installed)
+
                 {register ? (
                     <div className="form-container">
                         <form className="form">
@@ -45,7 +41,7 @@ function Login() {
                             <input placeholder="username" type="text"></input>
                             <input type="password" placeholder="password" />
                         </form>
-<<<<<<< HEAD
+
                         <button type="submit" className="action-button">
                             {' '}
               Login{' '}
@@ -53,17 +49,6 @@ function Login() {
                         <button className="swichter-button " onClick={() => swicht()}>
                             {register ? 'Sign In' : 'Sign Up!'}
                         </button>
-=======
-                        <div className="buttons-container">
-                            <div type="submit" className="action-button">
-                                {' '}
-                Login{' '}
-                            </div>
-                            <div className="swichter-button" onClick={() => swicht()}>
-                                {register ? 'Sign In' : 'Sign Up!'}
-                            </div>
-                        </div>
->>>>>>> 4ad3eda (axios-installed)
                     </div>
                 )}
             </div>
