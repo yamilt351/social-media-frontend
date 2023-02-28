7d87bc9275c093f4b42658341be7256b2a155087
yamil351 <leokasper@live.com.ar>
2023-02-28 15:46:53 -0300

    api-call-setup

--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,36 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './CardContainer.css'
 import Card from '../Card/card'
+import axios from 'axios'
+import { http } from '../ApiCall/FetchFunctions'
 /* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
 
 function CardContainer({ indicator }) {
+    const [article, setArticle] = useState([])
+
+    useEffect(() => {
+        const fetchArticles = async () => {
+            try {
+                const res = await axios.get(`${http}posts/random`)
+                setArticle(res.data)
+                console.log(article)
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
+        fetchArticles()
+    }, [])
     return (
         <>
             <h1 className="Page-indicator">{indicator}</h1>
             <div className="section-card-container">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {article.map(() => {
+                    <Card/>
+                })}
             </div>
         </>
     )
