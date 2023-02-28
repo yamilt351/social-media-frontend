ec1465111eb321e35d6b548b5d67db69848b7d56
yamil351 <leokasper@live.com.ar>
2023-02-28 20:28:54 -0300

    css

--- src/Components/LoginScreen/Login.jsx
+++ src/Components/LoginScreen/Login.jsx
@@ -14,30 +14,38 @@ function Login() {
     }
 
     return (
+<<<<<<< HEAD
         <section id="SignIn" className="SignIn">
             <div className="SignIn-Container">
                 <h1>Welcome!</h1>
+=======
+        <section id="SignIn" className="login-screen">
+            <div className="form-action-container">
+>>>>>>> 4ad3eda (axios-installed)
                 {register ? (
-                    <div className="form-container signUp">
+                    <div className="form-container">
                         <form className="form">
                             <input placeholder="E-mail" type="email"></input>
                             <input placeholder="Username" type="text"></input>
                             <input placeholder="Password" type="password" />
                         </form>
-                        <button type="submit" className="action-button">
-                            {' '}
-              Create User
-                        </button>
-                        <button className="swichter-button" onClick={() => swicht()}>
-                            {register ? 'Sign In' : 'Sign Up!'}
-                        </button>
+                        <div className="buttons-container">
+                            <div type="submit" className="action-button">
+                                {' '}
+                Create User
+                            </div>
+                            <div className="swichter-button" onClick={() => swicht()}>
+                                {register ? 'Sign In' : 'Sign Up!'}
+                            </div>
+                        </div>
                     </div>
                 ) : (
-                    <div className="form-container signIn">
+                    <div className="form-container">
                         <form className="form">
                             <input placeholder="username" type="text"></input>
                             <input type="password" placeholder="password" />
                         </form>
+<<<<<<< HEAD
                         <button type="submit" className="action-button">
                             {' '}
               Login{' '}
@@ -45,6 +53,17 @@ function Login() {
                         <button className="swichter-button " onClick={() => swicht()}>
                             {register ? 'Sign In' : 'Sign Up!'}
                         </button>
+=======
+                        <div className="buttons-container">
+                            <div type="submit" className="action-button">
+                                {' '}
+                Login{' '}
+                            </div>
+                            <div className="swichter-button" onClick={() => swicht()}>
+                                {register ? 'Sign In' : 'Sign Up!'}
+                            </div>
+                        </div>
+>>>>>>> 4ad3eda (axios-installed)
                     </div>
                 )}
             </div>
