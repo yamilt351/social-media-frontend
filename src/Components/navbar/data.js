c15a8a931907a9acfdb59d8070f49e21cf4c5cff
yamil351 <leokasper@live.com.ar>
2023-03-03 03:28:58 -0300

    rendering-navbar-if-userOnline

--- src/Components/navbar/data.js
+++ src/Components/navbar/data.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from "react";
 import {
-    FaArrowCircleLeft,
-    FaFire,
-    FaHome,
-    FaHeart,
-    FaSignInAlt,
-    FaUserAlt,
-    FaFeather,
-} from 'react-icons/fa'
+  FaArrowCircleLeft,
+  FaFire,
+  FaHome,
+  FaHeart,
+  FaSignInAlt,
+  FaUserAlt,
+  FaFeather,
+} from "react-icons/fa";
 
 export const data = [
-    { id: 2, name: 'Sign In', icon: <FaSignInAlt />, url: '/SignIn' },
-    { id: 1, name: 'Home', icon: <FaHome />, url: '/' },
-    { id: 3, name: 'Trending Post', icon: <FaFire />, url: '/Trending' },
-]
+  { id: 2, name: "Sign In", icon: <FaSignInAlt />, url: "/SignIn" },
+  { id: 1, name: "Home", icon: <FaHome />, url: "/" },
+  { id: 3, name: "Trending Post", icon: <FaFire />, url: "/Trending" },
+];
 
 export const loged = [
-    { id: 2, name: 'Subscriptions', icon: <FaHeart />, url: '/Subscriptions' },
-    { id: 1, name: 'Profile', icon: <FaUserAlt />, url: '/Profile' },
-    { id: 3, name: 'Create Post', icon: <FaFeather />, url: '/CreatePost' },
-    { id: 0, name: 'Log Out', icon: <FaArrowCircleLeft />, url: '/LogOut' },
-]
+  { id: 0, name: "Home", icon: <FaHome />, url: "/" },
+  { id: 1, name: "Trending Post", icon: <FaFire />, url: "/Trending" },
+
+  { id: 2, name: "Subscriptions", icon: <FaHeart />, url: "/Subscriptions" },
+  { id: 3, name: "Profile", icon: <FaUserAlt />, url: "/Profile" },
+  { id: 4, name: "Create Post", icon: <FaFeather />, url: "/CreatePost" },
+  { id: 5, name: "Log Out", icon: <FaArrowCircleLeft />, url: "/LogOut" },
+];
